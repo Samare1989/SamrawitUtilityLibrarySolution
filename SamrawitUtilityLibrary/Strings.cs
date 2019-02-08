b751b2a91bf23db1bf5a0d6906f2a97df2c1b702
sammy <samrawit.amare@ymail.com>
2019-02-07 21:38:16 -0500

    Samry

---
+++ SamrawitUtilityLibrary/Strings.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SamrawitUtilityLibrary
+{   
+
+    public class Strings { 
+
+
+        public static bool ValidUSZipCode(string ValidUSZipCode)
+    {
+        Regex regex = new Regex(@"^\d{ 5 }(-?\d{ 4})?");
+        var match = regex.IsMatch(ValidUSZipCode);
+        return match;
+    }
+
+
+        public static bool ValidUSPhoneNumber(string PhoneNumber)
+         {
+                 Regex regex = new Regex(@"^ (\d{3 } -)?\d{ 3} -\d{4}");
+                 var match = regex.IsMatch(PhoneNumber);
+                  return match;
+         }
+
+        public static bool EqualIgnoreCase(string strA, string strB)
+        {
+                var result = String.Compare(strA.ToUpper(), strB.ToUpper());
+                bool returnResult;
+                if(result== 0)
+            {
+                returnResult = true;
+            }
+            else
+            {
+                returnResult = false;
+            }
+            return returnResult;
+        }
+    }
+}
---
+++ TestSamrawitUtilityLibrary/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SamrawitUtilityLibrary;
+
+namespace TestSamrawitUtilityLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] zips = { "12345", "123456789", "12345-6789" };
+            foreach(var zip in zips)
+            {
+                Console.WriteLine($"{zip} is" + (Strings.ValidUSZipCode(zip) ? "" : "not") + "valid");
+            }
+
+           
+            var Phone1 = "322-8888";
+            var Phone1Valid = Strings.ValidUSPhoneNumber(Phone1);
+            if (Phone1Valid)
+            {
+                Console.WriteLine($"{Phone1} is valid");
+            }
+
+            else
+            {
+                Console.WriteLine($"{Phone1} is not valid");
+            }
+            var Phone2 = "513-322-8888";
+            var Phone2Valid = Strings.ValidUSPhoneNumber(Phone2);
+            if (Phone2Valid)
+            {
+                Console.WriteLine($"{Phone2} is valid");
+                }
+                else{
+                    Console.WriteLine($" {Phone2} is not vaild");
+                }
+
+            
+            
+          var result =  Strings.EqualIgnoreCase("ABC", "abc");
+            if (result == true)
+            {
+                Console.WriteLine("Equal");
+            }
+            else
+            {
+                Console.WriteLine("Not Equal");
+
+                 
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
+
+
+//// creating strings. inorder for strings to be equal they have to be in the same length.
